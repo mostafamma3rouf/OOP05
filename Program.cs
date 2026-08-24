605a221029de12942f360a85b85ec261f71356f3
mostafamma3rouf <mostafamohamedmarouf@gmail.com>
2026-08-24 22:52:53 +0300

    p1-q1-a

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-
+            #region Part 01 — Theoretical Questions
+            #region Q1 Object Copying
+            #region a) What happens when you assign one object variable to another object variable?
+            /*
+             * It takes a copy of it, and the difference lies in the type of data 
+             * because some will take the value and others will point to the reference.
+             */
+            #endregion
+            #endregion
+            #endregion
         }
     }
 }
