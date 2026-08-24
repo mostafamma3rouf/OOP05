c87db4a17ba0b7a9207a639deb53f09b1c61d542
mostafamma3rouf <mostafamohamedmarouf@gmail.com>
2026-08-24 23:08:21 +0300

    p1-q1-c

--- Program.cs
+++ Program.cs
@@ -17,6 +17,16 @@
             // No. Assigning one object to another usually does not create a new object.
             // Instead, both variables refer to the same object.
             #endregion
+
+            #region c) What is the difference between copying an object and copying its reference?
+            /*
+             * * copying   => take a copy of the value, and when you modify it, 
+                              only the copy is modified, and the original remains as is.
+
+             * * reference => You take a copy of the reference, and when making changes, 
+                              the changes are made to both the copy and the original.
+             */
+            #endregion
             #endregion
             #endregion
         }
